c07b4fa877edb7a5ac16b7c11480587e1b81d7b4
Rafi <mrkamin2@gmail.com>
2025-04-14 11:31:43 +0500

    Apply Dry and best practise method on the hero title component

--- app/components/HeroTitle.tsx
+++ app/components/HeroTitle.tsx
@@ -3,23 +3,22 @@ import { handleClick } from "../page";
 const HeroTitle = () => {
   return (
     <div className="hero-title-container">
-      <p className="hero-title-tag">
+      <h2 className="hero-title-tag">
         JUSTCOLD REFRIGERATION, MAINTENANCE & MORE
-      </p>
+      </h2>
 
-      <h1 className="hero-title-heading">
+      <h1 className="hero-title-heading" aria-label="JustCold Hero Tile">
         Keep Things <span className="hero-title-highlight">Fresh</span>
       </h1>
 
-      <p className="hero-title-description" style={{ whiteSpace: "pre-line" }}>
-        At JustCold Refrigeration LTD, we put our clients first by upholding 
-        the highest industry standards through expertise, courtesy, and professionalism. 
-        You&apos;ll be amazed by the results and glad you chose us— 
-        here are just some of our services:
+      <p className="hero-title-description">
+        &quot;At JustCold Refrigeration LTD, we prioritize our clients by delivering the highest industry standards—through expertise, courtesy, and professionalism.
+        You’ll be impressed by the results and confident you made the right choice.
+        Here’s a glimpse of what we offer:&quot;
       </p>
 
       <div>
-        <button className="estimate-btn"
+        <button className="estimate-btn pointer-events-auto"
         onClick={handleClick}>
           Get a Free Estimation
         </button>
